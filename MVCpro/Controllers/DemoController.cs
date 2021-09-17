9dffe8bcd22adb214ef4dffe2e38e41189430800
Guru Shruthy <gurushruthy.g@gmail.com>
2021-09-17 12:37:03 +0530

    first

---
+++ MVCpro/Controllers/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCpro.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.WelcomeMsg = "Welcome to Core MVC";
+            return View();
+        }
+        public ViewResult ShowFruits()
+        {
+            IEnumerable<string> names = new List<string>()
+            {
+                "Mango","Apple","Orange","Banana","Grapes"
+            };
+            return View(names);
+
+        }
+    }
+}
---
+++ MVCpro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCpro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
